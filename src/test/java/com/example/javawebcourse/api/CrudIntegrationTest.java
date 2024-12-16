74e160b859b3d1f736e3bce6967ba0a1746eedb0
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2024-12-16 23:06:27 +0200

    fixed migrations

--- src/test/java/com/example/javawebcourse/api/CrudIntegrationTest.java
+++ src/test/java/com/example/javawebcourse/api/CrudIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -25,6 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
+@Rollback
 public class CrudIntegrationTest {
 
     @Container
@@ -41,15 +43,6 @@ public class CrudIntegrationTest {
         registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-    }
-
-    @BeforeEach
-    void setUp() {
-        entityManager.createQuery("DELETE FROM OrderEntry").executeUpdate();
-        entityManager.createQuery("DELETE FROM Order").executeUpdate();
-        entityManager.createQuery("DELETE FROM Product").executeUpdate();
-        entityManager.createQuery("DELETE FROM Category").executeUpdate();
-        entityManager.createQuery("DELETE FROM CosmoCat").executeUpdate();
     }
 
     @Test
